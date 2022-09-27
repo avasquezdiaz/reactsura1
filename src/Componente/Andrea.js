06b3da7ba1770377298217ccad29d25b94beaef1
407 <407@academicos.cesde.edu.co>
2022-09-27 17:35:33 -0500

    se crea app de react

---
+++ src/Componente/Andrea.js
@@ -0,0 +1,14 @@
+export function Andrea () {
+
+    let nombre="Andrea Vasquez"
+
+    //interpolar-controlar la interfaz grafica desde js
+
+    return(
+       <>
+         <h1>Hola React soy {nombre}</h1>
+         <img ></img>
+        
+       </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Banner } from './Banner/Banner.js';
+import {Andrea} from './Componente/Andrea.js';
+import {Menu} from './Menu/Menu.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Menu></Menu>
+    <Banner></Banner>
+    <Andrea/>
+
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
